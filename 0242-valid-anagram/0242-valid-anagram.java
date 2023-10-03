5a705ddd506d1ca180c9e61325ff85d85e7a5acd
Govind Mudavadkar <89062303+Govindrm7@users.noreply.github.com>
2023-10-03 23:47:07 +0530

    Time: 6 ms (40.89%), Space: 41.6 MB (97.37%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] charCount = new int[26]; // Assuming lowercase English letters
+
+        for (int i = 0; i < s.length(); i++) {
+            charCount[s.charAt(i) - 'a']++;
+            charCount[t.charAt(i) - 'a']--;
+        }
+
+        for (int count : charCount) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
