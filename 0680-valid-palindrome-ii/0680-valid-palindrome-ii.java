8cbbc1d4945a8f98b99b36e42fe25127f4563f58
Govind Mudavadkar <89062303+Govindrm7@users.noreply.github.com>
2023-10-04 07:27:34 +0530

    Time: 6 ms (55.97%), Space: 44.7 MB (9.78%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                // Try deleting either the character at 'left' or 'right'.
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        
+        return true; // The string is already a palindrome or has become one after deletion.
+    }
+    
+    // Helper function to check if a string is a palindrome between indices 'left' and 'right'.
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
