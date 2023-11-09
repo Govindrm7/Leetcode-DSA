2601a0194c117f2f5c90e2be8dfa4c9652f6bc29
Govind Mudavadkar <89062303+Govindrm7@users.noreply.github.com>
2023-11-09 22:57:36 +0530

    Time: 2 ms (68.94%), Space: 45 MB (46.37%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,21 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int nonZeroIndex = 0;
-    for (int i = 0; i < nums.length; i++) {
-      if (nums[i] != 0) {
-        nums[nonZeroIndex] = nums[i];
-        nonZeroIndex++;
-      }
+        if(nums.length==1) return;
+        int j=-1;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==0){
+                j=i;
+                break;
+            }
+        }
+        if(j==-1) return;
+        for(int i=j+1; i<nums.length; i++){
+            if(nums[i]!=0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
+            }
+        }
     }
-
-    // Move the zeros to the end of the array.
-    for (int i = nonZeroIndex; i < nums.length; i++) {
-      nums[i] = 0;
-    }
-  }
 }
