70bf9bef9240a5cbfbbecf05ae209684c4793908
Govind Mudavadkar <89062303+Govindrm7@users.noreply.github.com>
2024-02-07 12:52:42 -0500

    Time: 0 ms (100.00%), Space: 42 MB (55.83%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n==0){
+            return 1;
+        }
+        else if(n<0){
+            return 1 / (x*myPow(x,-n-1));
+        }
+        else if(n%2==0){
+            double a = myPow(x,n/2);
+            return a*a;
+        }
+            return x*myPow(x,n-1); 
+    }
+}
+
